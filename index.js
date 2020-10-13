de579bbd2b22cfd20d0db2f772e0bf8336641bdf
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-10-13 04:41:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const lists = document.querySelectorAll('.ranked-list');
+  const list1 = lists[0];
+  const list2 = lists[1];
+  
+  let children = list1.children;
+  
+  for(let i = 0, l = children.length; i < l; i++) {
+    console.log("children " + i + " is " + children[i]);
+    children[i].innerHTML = parseInt(children[i]) + n;
+    }
+      
+  children = list2.children;
+  
+  for(let i = 0, l = children.length; i < l; i++) {
+    children[i].innerHTML = parseInt(children[i]) + n;
+    }
+      
+
+}
+
+function deepestChild() {
+  return document.querySelector('div#grand-node div div div div');
+}
