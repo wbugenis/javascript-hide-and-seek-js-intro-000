e36f7c3c20c8eced45ef5831752239044bcb025b
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-10-13 04:46:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const lists = document.querySelectorAll('.ranked-list');
+  const list1 = lists[0];
+  const list2 = lists[1];
+  
+  let child = list1.children;
+  
+  for(let i = 0, l = child.length; i < l; i++) {
+    child[i].innerHTML = parseInt(child[i].innerHTML) + n;
+    }
+      
+  child = list2.children;
+  
+  for(let i = 0, l = child.length; i < l; i++) {
+    child[i].innerHTML = parseInt(child[i].innerHTML) + n;
+    }
+}
+
+function deepestChild() {
+  return document.querySelector('div#grand-node div div div div');
+}
